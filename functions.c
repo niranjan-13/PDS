486bb2cd80715f8f9e9d71384e4c9db5c4cf02ec
Niranjan <pniranjan1303@gmail.com>
2023-03-02 11:28:20 +0530

    Added new function for Division operation

--- functions.c
+++ functions.c
@@ -3,7 +3,7 @@
 int mult(int a , int b);
 int add(int a , int b);
 int sub(int a , int b);
-
+int div(int a, int b);
 int main()
 {
 	int x,y;
@@ -14,11 +14,13 @@ int main()
 	int op =  mult( x, y) ;
 	int op1 =  add( x, y) ;
 	int op2 =  sub( x, y) ;
+	float op3 = div(x,y);
 
 
 	printf("%i\n",op);
 	printf("%i\n",op1);
 	printf("%i\n",op2);
+	printf("%f\n",op3);
 	return 0;
 }
 
@@ -38,4 +40,9 @@ int add(int a,int b)
 	int sub = a-b ;
 	return sub;
 
+}
+int div(int a,int b)
+{
+	int ans =a/b;
+	return ans;
 }
